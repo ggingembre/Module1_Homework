4dcdd336d1dbf6a8470f8312136aad970b8794cc
guillaume <ggingembre@gmail.com>
2017-06-08 23:33:00 +0300

    Commit 1 SQL assigment 1.1 with resource files

---
+++ src/main/resources/PopulateDB.sql
@@ -0,0 +1,267 @@
+CREATE DATABASE  IF NOT EXISTS `homework11` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `homework11`;
+-- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
+--
+-- Host: localhost    Database: homework11
+-- ------------------------------------------------------
+-- Server version	5.7.18-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `companies`
+--
+
+DROP TABLE IF EXISTS `companies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `companies` (
+  `company_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(250) NOT NULL,
+  `company_address` varchar(250) NOT NULL,
+  `company_description` varchar(250) NOT NULL,
+  PRIMARY KEY (`company_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `companies`
+--
+
+LOCK TABLES `companies` WRITE;
+/*!40000 ALTER TABLE `companies` DISABLE KEYS */;
+INSERT INTO `companies` VALUES (1,'IBM','Palo Alto, USA','International Business Machines, servers and clouds services'),(2,'VK','Moscow, Russia','Russian social media'),(3,'WebDesign','Kiev, Ukraine','website development');
+/*!40000 ALTER TABLE `companies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `companies_developers`
+--
+
+DROP TABLE IF EXISTS `companies_developers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `companies_developers` (
+  `developer_id` int(10) unsigned NOT NULL,
+  `company_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`developer_id`,`company_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `companies_developers_ibfk_1` FOREIGN KEY (`developer_id`) REFERENCES `developers` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `companies_developers_ibfk_2` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `companies_developers`
+--
+
+LOCK TABLES `companies_developers` WRITE;
+/*!40000 ALTER TABLE `companies_developers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `companies_developers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
+  `customer_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `customer_name` varchar(250) NOT NULL,
+  `customer_address` varchar(250) NOT NULL,
+  `customer_phone` varchar(250) NOT NULL,
+  `customer_description` varchar(250) NOT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (1,'Claude','Paris, France','+33145000000','French consultant'),(2,'Ambassade de France à Kiev','Kiev, Ukraine','French embassy in Kiev','+380440000'),(3,'Osmoun','Tashkent, Uzbekistan','+3000000','Uzbek tour operator');
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customers_projects`
+--
+
+DROP TABLE IF EXISTS `customers_projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers_projects` (
+  `customer_id` int(10) unsigned NOT NULL,
+  `project_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`customer_id`,`project_id`),
+  KEY `project_id` (`project_id`),
+  CONSTRAINT `customers_projects_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON UPDATE CASCADE,
+  CONSTRAINT `customers_projects_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers_projects`
+--
+
+LOCK TABLES `customers_projects` WRITE;
+/*!40000 ALTER TABLE `customers_projects` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customers_projects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `developers`
+--
+
+DROP TABLE IF EXISTS `developers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `developers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(150) NOT NULL,
+  `lastName` varchar(150) NOT NULL,
+  `gender` varchar(1) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `phone` varchar(150) NOT NULL,
+  `jobTitle` varchar(150) NOT NULL,
+  `salary` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `developers`
+--
+
+LOCK TABLES `developers` WRITE;
+/*!40000 ALTER TABLE `developers` DISABLE KEYS */;
+INSERT INTO `developers` VALUES (1,'Guillaume','G','M','Kiev, Ukraine','gg@gg.com','+38044000000','JAVA developer',2000),(2,'Elena','G','F','Kiev, Ukraine','eg@eg.com','+3804400000','Web developer',1500),(4,'Igor','G','M','Kiev, Ukraine','igor@igor.com','+380441111111','Senior Java Team Leader',4000);
+/*!40000 ALTER TABLE `developers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `developers_skills`
+--
+
+DROP TABLE IF EXISTS `developers_skills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `developers_skills` (
+  `developer_id` int(10) unsigned NOT NULL,
+  `skill_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`developer_id`,`skill_id`),
+  KEY `skill_id` (`skill_id`),
+  CONSTRAINT `developers_skills_ibfk_1` FOREIGN KEY (`developer_id`) REFERENCES `developers` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `developers_skills_ibfk_2` FOREIGN KEY (`skill_id`) REFERENCES `skills` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `developers_skills`
+--
+
+LOCK TABLES `developers_skills` WRITE;
+/*!40000 ALTER TABLE `developers_skills` DISABLE KEYS */;
+/*!40000 ALTER TABLE `developers_skills` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `project_developers`
+--
+
+DROP TABLE IF EXISTS `project_developers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `project_developers` (
+  `project_id` int(10) unsigned NOT NULL,
+  `developer_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`project_id`,`developer_id`),
+  KEY `developer_id` (`developer_id`),
+  CONSTRAINT `project_developers_ibfk_1` FOREIGN KEY (`developer_id`) REFERENCES `developers` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `project_developers_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project_developers`
+--
+
+LOCK TABLES `project_developers` WRITE;
+/*!40000 ALTER TABLE `project_developers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `project_developers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects`
+--
+
+DROP TABLE IF EXISTS `projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `projects` (
+  `project_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `project_name` varchar(250) NOT NULL,
+  `project_description` varchar(250) NOT NULL,
+  PRIMARY KEY (`project_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projects`
+--
+
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+INSERT INTO `projects` VALUES (1,'Pay System','development of salary payment system'),(2,'Site Dev','development of 10 sites for one client'),(3,'Analysis tool','development of system to analyse data');
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `skills`
+--
+
+DROP TABLE IF EXISTS `skills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `skills` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `skillName` varchar(250) NOT NULL,
+  `skillDescription` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `skills`
+--
+
+LOCK TABLES `skills` WRITE;
+/*!40000 ALTER TABLE `skills` DISABLE KEYS */;
+INSERT INTO `skills` VALUES (1,'Java','Java development'),(2,'web','web development'),(3,'Python','Python development');
+/*!40000 ALTER TABLE `skills` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-06-08 21:45:10
