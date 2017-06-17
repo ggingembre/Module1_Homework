f747162e3e224355b71c636b7b1665f440ea5b8f
guillaume <ggingembre@gmail.com>
2017-06-17 21:06:26 +0300

    Commit 5 assignment 2

--- src/main/resources/5.sql
+++ src/main/resources/5.sql
@@ -1,5 +1,29 @@
 -- Найти клиента (customer), которая приносит меньше всего прибыли компании (company) для каждой из компаний .
+-- first, for each company, find the project with least profit
+
 USE homework11;
+
+-- the problem with this query is that I do not know how to select the min profit per company
+-- also, I do not know how to sum profit if one client has more than one project
+-- finally, for some reason, the sorting by profit does not work
+
+SELECT
+  company_name,
+  customer_name,
+  project_name,
+  project_description,
+  (cost - SUM(developers.salary)) AS 'Profit'
+
+FROM projects
+  INNER JOIN project_developers ON projects.project_id = project_developers.project_id
+  INNER JOIN developers ON project_developers.developer_id = developers.id
+  INNER JOIN companies_developers ON developers.id = companies_developers.developer_id
+  INNER JOIN companies ON companies_developers.company_id = companies.company_id
+  INNER JOIN customers_projects ON projects.project_id = customers_projects.project_id
+  INNER JOIN customers ON customers_projects.customer_id = customers.customer_id
+
+GROUP BY companies.company_id, customers.customer_id, projects.project_id
+ORDER BY company_name ASC, customer_name, 'Profit' DESC;
 
 SELECT
   -- companies.company_id,
@@ -30,3 +54,4 @@ FROM
 
 GROUP BY customers.customer_id, projects.project_id, companies.company_id
 ORDER BY `profit` DESC;
+
--- src/main/resources/test2.sql
+++ src/main/resources/test2.sql
@@ -59,6 +59,25 @@ WHERE (cost - SUM(developers.salary)) = (SELECT
                 INNER JOIN project_developers ON projects.project_id = project_developers.project_id
                 INNER JOIN developers ON project_developers.developer_id = developers.id
 
-              GROUP BY projects.project_id) -- here I want to add the developers table
+              GROUP BY projects.project_id
+              ORDER BY 'Profit' ASC) -- here I want to add the developers table
 GROUP BY projects.project_id
 ORDER BY 'Profit' ASC;
+
+SELECT
+  company_name,
+  customer_name,
+  project_name,
+  project_description,
+  (cost - SUM(developers.salary)) AS 'Profit'
+
+FROM projects
+  INNER JOIN project_developers ON projects.project_id = project_developers.project_id
+  INNER JOIN developers ON project_developers.developer_id = developers.id
+  INNER JOIN companies_developers ON developers.id = companies_developers.developer_id
+  INNER JOIN companies ON companies_developers.company_id = companies.company_id
+  INNER JOIN customers_projects ON projects.project_id = customers_projects.project_id
+  INNER JOIN customers ON customers_projects.customer_id = customers.customer_id
+
+GROUP BY companies.company_id, customers.customer_id, projects.project_id
+ORDER BY company_name ASC, customer_name, 'Profit' DESC;
